67eaf11564eea4a8fc7bb2cf10925677b8a360a6
suhye0n <contact.suhyeon@gmail.com>
2023-11-01 10:51:44 +0900

    chore: 스프링부트 프로젝트 세팅

---
+++ server/src/main/java/com/subsidy/server/security/JwtAuthenticationFilter.java
@@ -0,0 +1,60 @@
+package com.subsidy.server.security;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter{
+	@Autowired
+	private TokenProvider tokenProvider;
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, 
+			FilterChain filterChain) throws ServletException, IOException {
+		try {
+			String token =parseBearerToken(request);
+			
+			if(token !=null &&!token.equalsIgnoreCase("null")){
+				String userId = tokenProvider.validateAndGetUserId(token);
+				log.info("인증된 유저 ID: "+ userId);
+				AbstractAuthenticationToken authentication = 
+						new UsernamePasswordAuthenticationToken(userId,null, AuthorityUtils.NO_AUTHORITIES);
+				
+			
+			authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+			SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+			securityContext.setAuthentication(authentication);
+			SecurityContextHolder.setContext(securityContext);
+			}
+			
+		} catch(Exception ex){
+			logger.error("인증 실패:", ex);
+			
+		}
+		filterChain.doFilter(request, response);
+	}
+	
+	private String parseBearerToken(HttpServletRequest request){
+		String bearerToken = request.getHeader("Authorization");
+		
+		if(StringUtils.hasText(bearerToken)&& bearerToken.startsWith("Bearer")){
+			return bearerToken.substring(7);
+		}
+		return null;
+	}
+}
